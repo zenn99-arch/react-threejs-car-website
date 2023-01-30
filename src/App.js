3227dfd1b0a9ae0bb66824d61f2879cb6cd03104
Bimal Ray <72511459+zenn99-arch@users.noreply.github.com>
2023-01-30 08:06:32 +0530

    set up

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+import { OrbitControls, PerspectiveCamera, SpotLight } from "@react-three/drei";
+import { Ground } from "./Ground";
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <color args={[0, 0, 0]} attach="background" />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.001}
+      />
+      
+      <Ground />
+    </>
+  );
+}
+
+const App = () => {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
+  );
+};
+
+export default App;
